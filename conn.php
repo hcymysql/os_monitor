69ce9da69aa4f6f2c53342442520c8a5aeada3ea
hcymysql <chunyang_he@139.com>
2020-04-07 13:44:02 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,7 @@
 <?php         
-     $conn = mysqli_connect("127.0.0.1","admin","hechunyang","os_monitor_db","3306") or die("数据库链接错误".mysql_error());  
+     $conn = mysqli_connect("127.0.0.1","admin","hechunyang","os_monitor_db","3306") or die("数据库链接错误" . PHP_EOL .mysqli_connect_error());
+     if ($conn){
+    	     echo "连接成功". PHP_EOL;
+     } 
      mysqli_query($conn,"set names utf8"); 
 ?>  
