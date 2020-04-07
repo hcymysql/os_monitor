06269a7bb7720bd2ff403bd46bbf93877d472359
hcymysql <chunyang_he@139.com>
2020-04-07 13:35:31 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,16 +1,4 @@
-    <?php   
-      
-     $conn = mysqli_connect("10.10.159.31","admin","hechunyang","sql_db","3306") or die("数据库链接错误".mysql_error());  
+<?php         
+     $conn = mysqli_connect("127.0.0.1","admin","hechunyang","os_monitor_db","3306") or die("数据库链接错误".mysql_error());  
      mysqli_query($conn,"set names utf8"); 
-
-/*
-	$local_host='10.10.159.31'; 
-	$result = mysqli_query($conn,"select id from os_status_info where host='${local_host}'");
-$row=mysqli_fetch_array($result,MYSQLI_NUM);
-
-if (!$result) {
-    printf("Error: %s\n", mysqli_error($conn));
-    exit();
-}
-*/
-    ?>  
+?>  
